2cd4c836f497e32a315ecd2476b511ea85989624
c.ilgner <c.ilgner@tmg-web.de>
2023-10-24 10:33:32 +0200

    ADD: Checks

--- src/main/java/de/goldendeveloper/Main.java
+++ src/main/java/de/goldendeveloper/Main.java
@@ -2,31 +2,13 @@ package de.goldendeveloper;
 
 import org.kohsuke.github.*;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
 
 public class Main {
 
-    private static GitHub github;
-    private static GHOrganization gdOrganization;
-
-    public static void main(String[] args) {
-        try {
-            github = GitHub.connect("Coho04", "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");
-            gdOrganization = github.getOrganization("Golden-Developer");
-            processRepositories();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void processRepositories() throws IOException {
+    public static void main(String[] args) throws IOException {
+        GitHub github = GitHub.connect("Coho04", "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");
+        GHOrganization gdOrganization = github.getOrganization("Golden-Developer");
         RepositoryProcessor processor = new RepositoryProcessor();
         for (GHRepository repo : gdOrganization.listRepositories()) {
             processor.process(repo);
--- src/main/java/de/goldendeveloper/RepositoryProcessor.java
+++ src/main/java/de/goldendeveloper/RepositoryProcessor.java
@@ -7,30 +7,33 @@ import org.kohsuke.github.GHRepository;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
 public class RepositoryProcessor {
+
 
     public void process(GHRepository repo) throws IOException {
         System.out.println("Processing " + repo.getName() + "...");
 
         checkOrUpload(repo, ".github/ISSUE_TEMPLATE", "bug_report.md", "Create bug_report.md", "src/main/resources/.github/ISSUE_TEMPLATE/bug_report.md");
         checkOrUpload(repo, ".github/ISSUE_TEMPLATE", "feature_request.md", "Create feature_request.md", "src/main/resources/.github/ISSUE_TEMPLATE/feature_request.md");
-//        checkOrUpload(repo, ".github", "dependabot.yml", "Create dependabot.yml", "src/main/resources/dependabot.yml"); //TODO: Issue
+
         checkOrUpload(repo, ".github", "PULL_REQUEST_TEMPLATE.md", "Create PULL_REQUEST_TEMPLATE.md", "src/main/resources/.github/PULL_REQUEST_TEMPLATE.md");
         checkOrUpload(repo, "", "CODE_OF_CONDUCT.md", "Create CODE_OF_CONDUCT.md", "src/main/resources/CODE_OF_CONDUCT.md");
         checkOrUpload(repo, "", "CONTRIBUTING.md", "Create CONTRIBUTING.md", "src/main/resources/CONTRIBUTING.md");
         checkOrUpload(repo, "", "LICENSE", "Create LICENSE", "src/main/resources/LICENSE");
         checkOrUpload(repo, "", "SECURITY.md", "Create SECURITY.md", "src/main/resources/SECURITY.md");
 
+        checkOrIssue(repo, ".github", "dependabot.yml", "Create dependabot.yml");
+
         if (repo.getLanguage() != null) {
-            System.out.println("Programming Language: " + repo.getLanguage());
             if (repo.getLanguage().equalsIgnoreCase("Java")) {
 //                checkDirectoryNotExistsOrMakeIssue(repo, ".idea", "Remove .idea directory");
             }
         }
+        checkWebsite(repo);
+        checkTopics(repo);
         System.out.println("Finished processing " + repo.getName() + "!");
     }
 
@@ -57,8 +60,27 @@ public class RepositoryProcessor {
         }
     }
 
+    private void checkOrIssue(GHRepository repo, String directoryPath, String fileName, String issueTitle) throws IOException {
+        try {
+            List<GHContent> directory = repo.getDirectoryContent(directoryPath);
+            if (directory.isEmpty() || directory.stream().noneMatch(file -> file.getName().equals(fileName))) {
+                if (directoryPath.isEmpty()) {
+                    makeIssue(repo, issueTitle);
+                } else {
+                    makeIssue(repo, issueTitle);
+                }
+            }
+        } catch (Exception e) {
+            if (e.getMessage().contains("Not Found")) {
+                makeIssue(repo, issueTitle);
+            } else {
+                System.out.println("Error in " + repo.getName() + ": " + e.getMessage());
+            }
+        }
+    }
+
     public void uploadFile(GHRepository repo, String content, String commit, String path) throws IOException {
-        if (!repo.isArchived()) {
+        if (repoIsNotIgnored(repo)) {
             GHContentBuilder contentBuilder = repo.createContent();
             contentBuilder.branch("main");
             contentBuilder.content(content);
@@ -69,17 +91,66 @@ public class RepositoryProcessor {
         }
     }
 
-    public String readResource(String localPath) {
-        Path path = Paths.get(localPath);
-        try {
-            StringBuilder builder = new StringBuilder();
-            for (String line : Files.readAllLines(path)) {
-                builder.append(line).append("\n");
-            }
-            return builder.toString();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public void makeIssue(GHRepository repo, String title) throws IOException {
+        if (repoIsNotIgnored(repo) && repo.getIssues(GHIssueState.OPEN).stream().noneMatch(issue -> issue.getTitle().equals(title))) {
+            repo.createIssue(title).assignee("Coho04").create();
+        } else if (!repoIsNotIgnored(repo) && repo.getIssues(GHIssueState.ALL).stream().anyMatch(issue -> issue.getTitle().equals(title))) {
+            repo.getIssues(GHIssueState.OPEN).stream().filter(issue -> issue.getTitle().equals(title)).forEach(issue -> {
+                try {
+                    if (!repo.isArchived()) {
+                        issue.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
         }
-        return null;
+    }
+
+    public void checkWebsite(GHRepository repo) throws IOException {
+        if (repoIsNotIgnored(repo) && repo.getHomepage() == null) {
+            repo.setHomepage("https://Golden-Developer.de");
+        }
+    }
+
+    public void checkTopics(GHRepository repo) throws IOException {
+        if (repoIsNotIgnored(repo)) {
+            List<String> defaultTopics = List.of("golden-developer");
+            List<String> topics = repo.listTopics();
+            for (String topic : defaultTopics) {
+                if (!topics.contains(topic)) {
+                    topics.add(topic);
+                }
+            }
+            if (repo.getLanguage() != null && !topics.contains(repo.getLanguage().toLowerCase())) {
+                topics.add(repo.getLanguage().toLowerCase());
+            }
+            repo.setTopics(topics);
+        }
+    }
+
+    public String readResource(String localPath) throws IOException {
+        StringBuilder builder = new StringBuilder();
+        for (String line : Files.readAllLines(Paths.get(localPath))) {
+            builder.append(line).append("\n");
+        }
+        return builder.toString();
+    }
+
+    public Boolean repoIsNotIgnored(GHRepository repo) {
+        List<String> ignoredRepos = new java.util.ArrayList<>(List.of("TeamSpeakServerInstallScript", "MinecraftInstallScript", ".github", ".github-private"));
+        List<String> ignoredLanguages = new java.util.ArrayList<>(List.of("html", "css", "typescript", "shell"));
+        ignoredRepos.replaceAll(String::toLowerCase);
+        ignoredLanguages.replaceAll(String::toLowerCase);
+        if (repo.isArchived()) {
+            return false;
+        }
+        if (repo.getLanguage() == null) {
+            return false;
+        }
+        if (ignoredLanguages.contains(repo.getLanguage().toLowerCase())) {
+            return false;
+        }
+        return !ignoredRepos.contains(repo.getName().toLowerCase());
     }
 }
